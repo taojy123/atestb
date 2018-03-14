4a5475c1f1e5d620bbfac57b9524a4682f3d9f86
taojy123 <taojy123@163.com>
2018-03-14 17:02:39 +0800

    change

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from bottle import route, run, template
 
 @route('/hello/<name>')
 def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+    return template('<b>Hello {{name}}</b>!!!!', name=name)
 
 run(host='0.0.0.0', port=8080)
 
